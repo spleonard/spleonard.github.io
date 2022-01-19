43222d3c00b93bff61754ef2691404fb921ff2e7
spleonard <18sleonard@gmail.com>
2022-01-19 15:52:57 -0500

    Big Update

--- js/docs.js
+++ js/docs.js
@@ -1,12 +1,29 @@
-
+/* This function saves the name of the clicked button */
 function ButtonX_Click(pageRequest) {
-    console.log(pageRequest + " was the selection");
+    /* Saves and logs the selected value as a local variable*/
     localStorage.setItem("recipeSelected",pageRequest);
+    console.log(pageRequest + " was the selection");
 
-    const fullRecipeJson = localStorage.getItem("recipeJson")
-    const recipeTitle = JSON.parse(fullRecipeJson);
-    console.log(recipeTitle);
+    /* Updates other local variables */
+    updateVariables();
+}
+/* This function saves all subset categories of the Json file as local variables */
+function updateVariables() {
+    /* Call and log the full Json file (formed in recipeJson.js) */
+    const fullRecipeJson = localStorage.getItem("recipeJson");
+    const parsedRecipeJson = JSON.parse(fullRecipeJson);
 
-    let data = recipeTitle[pageRequest].name;
-    console.log(data);
+    /* Retrieve the recipe name and simplify the Json file to only include relevant data */
+    const recipeChosen = localStorage.getItem("recipeSelected");
+    let RJson = parsedRecipeJson[recipeChosen];
+
+    /* Save all relevant data as new local variables */
+    localStorage.setItem("recipeIngredients",RJson.ingredients);
+    console.log(RJson.ingredients);
+    localStorage.setItem("recipeLink",RJson.link);
+    localStorage.setItem("recipeMaterials",RJson.materials);
+    localStorage.setItem("recipeName",RJson.name);
+    localStorage.setItem("recipeServings",RJson.servings);
+    localStorage.setItem("recipeSteps",RJson.steps);
+    localStorage.setItem("recipeTime",RJson.time);
 }
--- js/recipeJson.js
+++ js/recipeJson.js
@@ -1,30 +1,194 @@
 const recipeJSON = `
         {
-            "Macaron": {
-                    "name": "its a macaron cool!",
-                    "job": "CS",
-                    "age": 9,
-                    "country": "USA"
+            "Apple Rosemary Bread": {
+                    "ingredients": "example example",
+                    "link": "example2",
+                    "materials": "example3",
+                    "name": "example4",
+                    "servings": "example5",
+                    "steps": "example6",
+                    "time": "example7"
+                },
+            "Apple Sauce (Dad’s Variant)": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Banana Bread": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Bagels": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Cinnamon Rolls": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Coffee Cake": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Coffee Cake": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Cookies (Meg’s Variant)": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Cookies (Vegan Variant)": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Crepes": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Crepes (Alison’s Variant)": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Croissant": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Garlic Bread": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
                 },
             "Lava Cake": {
-                    "name": "Lava Cake",
-                    "job": "CS3",
-                    "age": 93,
-                    "country": "USA3"
-            },
-            "Wings": {
-                    "name": "Wings",
-                    "job": "CS3",
-                    "age": 93,
-                    "country": "USA3"
-            },
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Lava Cake (Paloma’s Variant)": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Macaron": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Monkey Bread": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Pizza": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
             "Ribs": {
-                    "name": "Ribs",
-                    "job": "CS3",
-                    "age": 93,
-                    "country": "USA3"
-            }
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Rolls": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                },
+            "Wings": {
+                    "ingredients": "",
+                    "link": "",
+                    "materials": "",
+                    "name": "",
+                    "servings": "",
+                    "steps": "",
+                    "time": ""
+                }
         }
         `;
-        
 localStorage.setItem("recipeJson",recipeJSON);
