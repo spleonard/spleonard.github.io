cf064c55ab5a5262e91c0627d12e364c221da834
spleonard <18sleonard@gmail.com>
2022-05-10 17:02:19 -0400

    Added Quixx

---
+++ Quixx/checkbox.js
@@ -0,0 +1,51 @@
+function checkfunction(a,b) {
+
+    var check = document.getElementById(a);
+    var cross = document.getElementById(b);
+    if(check.checked == true){
+        cross.style.display = "flex";
+    }
+    else {
+        cross.style.display = "none";
+    }
+}
+
+
+
+function tallyChecks(cname,scoreBox){
+    var inputElems = document.getElementsByTagName("input");
+    count = 0;
+    for (var i=0; i<inputElems.length; i++) {
+    if (inputElems[i].type === "checkbox" && inputElems[i].className === cname && inputElems[i].checked === true){
+        count++;
+    }
+}
+var score = document.getElementById(scoreBox);
+score.innerHTML = count*(count+1)/2;
+finalScore();
+}
+
+function tallyPenaltyChecks(cname){
+    var inputElems = document.getElementsByTagName("input");
+    count = 0;
+    for (var i=0; i<inputElems.length; i++) {
+    if (inputElems[i].type === "checkbox" && inputElems[i].className === cname && inputElems[i].checked === true){
+        count++;
+    }
+}
+var score = document.getElementById("penaltyScore");
+score.innerHTML = count*5;
+finalScore();
+}
+
+
+
+function finalScore(){
+    var redScore = document.getElementById("redScore");
+    var yellowScore = document.getElementById("yellowScore");
+    var greenScore = document.getElementById("greenScore");
+    var blueScore = document.getElementById("blueScore");
+    var penaltyScore = document.getElementById("penaltyScore");
+    var finalScore = document.getElementById("finalScore");
+    finalScore.innerHTML = parseInt(redScore.innerHTML) + parseInt(yellowScore.innerHTML) + parseInt(greenScore.innerHTML) + parseInt(blueScore.innerHTML) - parseInt(penaltyScore.innerHTML);
+}
