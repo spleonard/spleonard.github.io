d04d381cfcf6c7c95efe3949a2c61fb8a9474a75
spleonard <18sleonard@gmail.com>
2022-12-30 16:49:16 -0500

    Checkbox Functionality Implemented

--- FishFinder/js/mainFunctions.js
+++ FishFinder/js/mainFunctions.js
@@ -1,19 +1,58 @@
+
+var lastButtonEntry = "all";
+var activeFilters = [];
 filterSelection("all")
-var lastButtonEntry = "all"
+
 
 function filterSelection(c) {
+  //Append all Clicked to a List
+  if (c != "all") {
+    buttonPushed = document.getElementById(c);
+    if (buttonPushed.classList.contains("active")==false) {
+      w3AddClass(document.getElementById(c), "active");
+      activeFilters.push(c);
+    }
+    else {
+      w3RemoveClass(buttonPushed, "active");
+      cIndex = activeFilters.indexOf(c);
+      activeFilters.splice(cIndex, 1)
+    }
+    console.log(activeFilters);
+  }
+  console.log(activeFilters.length)
+  filterSelection2(c);
+}
+
+function checkAllTags(xi) {
+  var tagsInElement = 0;
+  for (i = 0; i < activeFilters.length; i++) {
+    if (xi.className.indexOf(activeFilters[i]) > -1) tagsInElement++;
+  }
+  if (tagsInElement == activeFilters.length) return true;
+  return false;
+}
+
+
+function filterSelection2(c) {
+  // Store Last Click for CheckMark Updates
   if (c == "useLast") c = lastButtonEntry;
   lastButtonEntry = c;
-  var x, i, s4;
+  if (c == "all") c = "";
+
+  var x, i, s4, hP, sT;
+
+  // Check Toggle on Checkboxes
   x = document.getElementsByClassName("filterDiv");
   s4 = document.getElementById('hide4seasons'); s4 = s4.checked;
   hP = document.getElementById('hidePot'); hP = hP.checked;
   sT = document.getElementById('showTimes'); sT = sT.checked;
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    if (checkAllTags(x[i])) w3AddClass(x[i], "show");
     // Below Lines Remove Divs Based on Checkbox Toggles
     if (x[i].classList.contains("spring") & x[i].classList.contains("summer") & x[i].classList.contains("fall") & x[i].classList.contains("winter") & s4) w3RemoveClass(x[i], "show");
     if ((x[i].classList.contains("pot") || x[i].classList.contains("plant"))  & hP) w3RemoveClass(x[i], "show");
@@ -59,3 +98,18 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function clearFilters() {
+  x = document.getElementsByClassName("btn");
+  for (i = 0; i < x.length; i++) {   
+    w3RemoveClass(x[i], "active");
+    cIndex = activeFilters.indexOf(x[i]);
+    activeFilters.splice(cIndex, 1)
+  }
+
+  y = document.getElementsByClassName("switch");
+  for (i = 0; i < y.length; i++) {   
+      y[i].checked = false;
+    }
+}
+
