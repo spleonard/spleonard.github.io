e656b9d38423b75c59ba7018a70f7084ff032c8c
spleonard <62518016+spleonard@users.noreply.github.com>
2025-04-07 21:04:44 -0400

    clue improved alot

--- Clue/boxMarker.js
+++ Clue/boxMarker.js
@@ -1,5 +1,41 @@
 let selectedColor = '';
-document.getElementById('boxes').innerHTML = '<div class="color-box" onclick="colorBox(this)"></div>'.repeat(105);
+document.getElementById('boxes').innerHTML = 
+'<input class="color-box" type="text" onclick="colorBox(this)">'.repeat(126)+
+
+'<div class="main-title">CLUE</div>'+
+'<input class="playername" type="text">'+
+'<input class="playername" type="text">'+
+'<input class="playername" type="text">'+
+'<input class="playername" type="text">'+
+'<input class="playername" type="text">'+
+'<input class="playername" type="text">'+
+
+'<div class="section-title">SUSPECTS</div>'+
+'<div class="choice">Colonel Mustard</div>'+
+'<div class="choice">Professor Plum</div>'+
+'<div class="choice">Reverend Green</div>'+
+'<div class="choice">Mr. Peacock</div>'+
+'<div class="choice">Miss Scarlet</div>'+
+'<div class="choice">Mrs. White</div>'+
+
+'<div class="section-title">WEAPONS</div>'+
+'<div class="choice">Knife</div>'+
+'<div class="choice">Candlestick</div>'+
+'<div class="choice">Revolver</div>'+
+'<div class="choice">Rope</div>'+
+'<div class="choice">Lead Pipe</div>'+
+'<div class="choice">Wrench</div>'+
+
+'<div class="section-title">ROOMS</div>'+
+'<div class="choice">Hall</div>'+
+'<div class="choice">Lounge</div>'+
+'<div class="choice">Dining Room</div>'+
+'<div class="choice">Kitchen</div>'+
+'<div class="choice">Ballroom</div>'+
+'<div class="choice">Conservatory</div>'+
+'<div class="choice">Billard Room</div>'+
+'<div class="choice">Library</div>'+
+'<div class="choice">Study</div>';
 
 // Function to set the selected color
 function setColor(color) {
